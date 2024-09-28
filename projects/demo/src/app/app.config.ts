629827e5307266e0af14f211ff9a7376e317c178
Laszlo Batori <difecto@gmail.com>
2024-09-28 15:10:14 +0200

    Environments and init translate

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -5,9 +5,10 @@ import { routes } from './app.routes';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, `${environment.baseUrl}/assets/i18n/`);
 }
 
 export const appConfig: ApplicationConfig = {
